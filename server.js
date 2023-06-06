3a40c6c833175ca2c9631982677d0aa7f88e4054
gench <irresjeny@gmail.com>
2023-06-06 17:13:09 +0300

    cors update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const testRoutes = require('./routes/test-routes');
 const userRoutes = require('./routes/user-routes');
 const adminRoutes = require('./routes/admin-routes');
@@ -8,6 +9,7 @@ const teacherMaterials = require('./routes/teacher-routes');
 require('dotenv').config()
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose
     .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
